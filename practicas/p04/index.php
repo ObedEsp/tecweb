19f86eafdb15acb280e190c5869edc06175cc00e
ObedEsp <obedespinosa55@gmail.com>
2025-02-04 20:39:16 -0600

    Ejercicio 1 con modificaciones

---
+++ practicas/p04/index.php
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Práctica 4</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+    <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
+    <?php
+        // Declaración de variables
+        $_myvar = "valor1";
+        $_7var = "valor2";
+        //myvar;       // Inválida
+        $myvar = "valor3";
+        $var7 = "valor4";
+        $_element1 = "valor5";
+        //$house*5;     // Inválida
+        
+        echo '<h4>Respuesta:</h4>';   
+    
+        echo '<ul>';
+        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
+        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
+        echo '<li>myvar es inválida porque no tiene el signo de dólar ($).</li>';
+        echo '<li>$myvar es válida porque inicia con una letra.</li>';
+        echo '<li>$var7 es válida porque inicia con una letra.</li>';
+        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
+        echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
+        echo '</ul>';
+
+        //Se añadió la declaración XML.
+        //Se asignaron valores a las variables para evitar advertencias.
+    ?>
+</body>
+</html>
