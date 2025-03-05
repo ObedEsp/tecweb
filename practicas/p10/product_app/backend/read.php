9aa4bf1145f28470631c72f8120d1d26025a9da2
ObedEsp <obedespinosa55@gmail.com>
2025-03-05 12:47:03 -0600

    Se modifico read.php y datebase.php ejercicio 2

--- practicas/p10/product_app/backend/read.php
+++ practicas/p10/product_app/backend/read.php
@@ -3,27 +3,36 @@
 
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
-            // SE OBTIENEN LOS RESULTADOS
-			$row = $result->fetch_array(MYSQLI_ASSOC);
 
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = $value; // utf8_encode($value);
+    // SE VERIFICA QUE SE RECIBIÓ UN TÉRMINO DE BÚSQUEDA
+    if( isset($_POST['busqueda']) ) {
+        $busqueda = $_POST['busqueda'];
+
+        // SE REALIZA LA CONSULTA BUSCANDO POR ID, NOMBRE, MARCA O DETALLES
+        if ($result = $conexion->query("SELECT * FROM productos WHERE 
+        id LIKE '%{$busqueda}%' 
+        OR nombre LIKE '%{$busqueda}%' 
+        OR marca LIKE '%{$busqueda}%' 
+        OR detalles LIKE '%{$busqueda}%'")) {
+            // SE OBTIENEN TODOS LOS RESULTADOS
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $data[] = $row; // Se agregan todos los productos encontrados
+            }
+            if ($row) {
+                // SE ASEGURA QUE EL ID SE INCLUYA
+                $data['id'] = $row['id'];
+    
+                foreach ($row as $key => $value) {
+                    $data[$key] = $value;
                 }
             }
-			$result->free();
-		} else {
+            $result->free();
+        } else {
             die('Query Error: '.mysqli_error($conexion));
         }
-		$conexion->close();
+        $conexion->close();
     } 
-    
+
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
     echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
